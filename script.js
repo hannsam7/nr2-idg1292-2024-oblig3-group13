900e4fcd666639c28833e9b9ccbc34effd4e325d
Nora Storrø <norsto@stud.ntnu.no>
2024-05-02 12:01:22 +0200

    trying to link the js to the html

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //this is blatant copying, so idk if we can use this
 setTimeout(() => {
     document.querySelector().classList.add();
-}, 1000);
+}, 1000); //idk why we would need to time out something tbh.
 
 //elements for intersector to watch
-const somename1 = document.querySelector("somename1nameinhtml__start"); //nameinhtml refering to what class or id name an element has
+const kidFrame1 = document.querySelector(".kid1"); //nameinhtml refering to what class or id name an element has
 const somename2 = document.querySelector("somename2nameinhtml__start"); //looks like it needs a start point and end point
 //const somename3 = document.querySelector("somename3nameinhtml__start"); //related to observer3, might not get used
 
 const options = {
     rootMargin: "-20px",
-    threshold: [0, 0.25, 0.5, 0.75, 1] //array that makes different stages of zoom animation
+    threshold: [0, 0.25, 0.5, 0.75, 1] //array that makes different stages of zoom animation i think
 };
 
 const observer1 = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         //if intersecting, start somename1 
         if(entry.intersectionRatio > 0.5) {
-            document.querySelector("somename2nameinhtml").classList.add("somename2__closed"); //isk what the closed thing is about
+            document.querySelector(".kid1").classList.add("kid1"); //isk what the closed thing is about
             document.querySelector("somethingabouttitle").classList.remove("title__closed"); //idk what the title thing is about either
         }
     })
